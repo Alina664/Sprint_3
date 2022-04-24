4fe7d1d5f4fe087f23378a0038f44239817315b0
Alina Timofeeva <alina.timofeeva-2012@yandex.ru>
2022-04-25 02:37:40 +0400

    add registerCourier

--- src/main/java/RegisterNewCourier.java
+++ src/main/java/RegisterNewCourier.java
@@ -3,7 +3,6 @@ import lombok.Data;
 import org.apache.commons.lang3.RandomStringUtils;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 import static io.restassured.RestAssured.given;
 
@@ -13,18 +12,17 @@ public class RegisterNewCourier {
     private String login;
     private String password;
     private String firstName;
-    private HashMap<String, String> account = new HashMap<>();
 
     public RegisterNewCourier() {
     }
 
-    public RegisterNewCourier(HashMap<String, String> account) {
-        this.login = account.get("login");
-        this.password = account.get("password");
-        this.firstName = account.get("firstName");
+    public RegisterNewCourier(String login, String password, String firstName) {
+        this.login = login;
+        this.password = password;
+        this.firstName = firstName;
     }
 
-    public ArrayList<String> registerNewCourierAndReturnLoginPassword() {
+    public ArrayList<String> registerNewCourierAndReturnLoginPassword(){
 
         // с помощью библиотеки RandomStringUtils генерируем логин
         // метод randomAlphabetic генерирует строку, состоящую только из букв, в качестве параметра передаём длину строки
@@ -43,7 +41,7 @@ public class RegisterNewCourier {
                 + "\"firstName\":\"" + courierFirstName + "\"}";
 
         // отправляем запрос на регистрацию курьера и сохраняем ответ в переменную response класса Response
-        Response response = given()
+        Response response =  given()
                 .header("Content-type", "application/json")
                 .and()
                 .body(registerRequestBody)
